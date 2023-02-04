6bbc69e8a0c5507e4285c6b8cf7fc6ce8afe899e
limenez <limenez86@gmail.com>
2023-02-03 22:56:01 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function colorYellow(cor) {
+    var div = document.getElementById('caixa1')
+    div.style.backgroundColor = "yellow";
+}
+
+function colorOrange(cor) {
+    var div = document.getElementById('caixa2')
+    div.style.backgroundColor = "orange";
+}
+
+function colorBlue(cor) {
+    var div = document.getElementById('caixa3')
+    div.style.backgroundColor = "blue";
+}
